01ddb361561f211c8278d5140e409290934ba666
Andrew Romanov <anr@anadeainc.com>
2018-05-02 16:12:54 +0300

    Data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+BOOKS_TITLES = 20.times.map { Faker::Book.title }
+
+SEPARATOR = BOOKS_TITLES.length * 0.4
+
+BOOK_USERS = {
+  'anr@anadeainc.com' => BOOKS_TITLES[0, SEPARATOR],
+  'gpk@anadeainc.c0m' => BOOKS_TITLES[SEPARATOR..-1]
+}
+
+BOOK_USERS.each do |email, books|
+  user = User.new(email: email, password: 'secret')
+  books.each { |title| user.books.build(title: title) }
+  user.save
+end
