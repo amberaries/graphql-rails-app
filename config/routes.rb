d108172cdaab046eae436d0c2636dba83138ed6e
Andrew Romanov <anr@anadeainc.com>
2018-05-02 17:40:51 +0300

    Protect graphql route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'application#start'
 
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    authenticate :user do
+      mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    end
   end
 
   post "/graphql", to: "graphql#execute"
