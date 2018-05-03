bad2b79124d5fa5ce81834b810a317b6074d534a
Gregory Kravchenko <gpk@anahoret.com>
2018-05-03 11:18:14 +0300

    Create book mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -8,4 +8,13 @@ Types::MutationType = GraphQL::ObjectType.define do
       "Hello World!"
     }
   end
+
+  field :creatBook do
+    type Types::BookType
+    description "Get the book stuff"
+    argument :title, !types.String
+    resolve ->(root, args, ctx) {
+      Book.create(title: args["title"], user: ctx[:current_user])
+    }
+  end
 end
